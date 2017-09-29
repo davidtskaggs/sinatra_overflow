51863dc3d824e2f1c33bbf8698d68e2e4f85c0d9
Thierry Moudiki <email": null>
2017-09-29 15:57:05 -0400

    create models for answer, answer_comment and question_comment

---
+++ app/models/answer.rb
@@ -0,0 +1,7 @@
+class Answer < ActiveRecord::Base
+  # Remember to create a migration!
+  validates :content, presence: true
+  validates :author_id, presence: true
+  validates :question_id, presence: true
+  validates :comment_votes_id
+end
