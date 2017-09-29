630ff19f34793f7e11a9609c33223b54d7411fd5
Thierry Moudiki <email": null>
2017-09-29 16:22:04 -0400

    add validations to answer, comments, questions

--- app/models/answer_comment.rb
+++ app/models/answer_comment.rb
@@ -1,5 +1,8 @@
 class AnswerComment < ActiveRecord::Base
   # Remember to create a migration!
+  belongs_to :user
+  belongs_to :answer
+  belongs_to :comment_votes
   validates :content, presence: true
   validates :author_id, presence: true
   validates :answer_id, presence: true
