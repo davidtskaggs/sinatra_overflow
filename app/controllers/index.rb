bebc4252c44d372d6c2c6b471ea8680e03062586
Samory Harris <email": null>
2017-09-30 14:02:53 -0400

    Add seeds data

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,34 @@
-get '/' do
-  erb :index
-end
+# get '/' do
+#   erb :index
+# end
+
+# get '/' do
+#   redirect '/posts'
+# end
+
+# get '/posts' do
+#   @posts = Post.all
+#   erb :index
+# end
+
+# post '/posts/:id/vote' do
+#   post = Post.find(params[:id])
+#   post.votes.create(value: 1)
+#   redirect "/posts"
+# end
+
+# delete '/posts/:id' do
+#   # write logic for deleting posts here.
+# end
+
+# post '/posts' do
+#   Post.create( title: params[:title],
+#                username: Faker::Internet.free_email,
+#                comment_count: rand(1000) )
+#   redirect '/posts'
+# end
+
+# get '/post/:id' do
+#   @post = Post.find(params[:id])
+#   erb :post
+# end
---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+
+# 10.times do
+#   post = Post.create!( title: Faker::Company.catch_phrase,
+#                username: Faker::Internet.user_name,
+#                comment_count: rand(1000),
+#                created_at: Time.now - rand(20000))
+
+#   vote_count = rand(100)
+#   vote_count.times do
+#     post.votes.create!(value: 1)
+#   end
+# end
+
+10.times do
+  user = User.create!(
+  email: Faker::Internet.free_email,
+  password: Faker::Internet.password(10, 15)
+  )
+
+end
